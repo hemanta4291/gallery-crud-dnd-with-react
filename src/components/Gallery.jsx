6f1e12656d57b1b079a01e974075e8e9246a252a
hemanta4291 <hemanta25roy@gmail.com>
2023-11-06 17:05:56 +0600

    Design and Functionaly Update

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,13 +1,33 @@
 // src/components/Gallery.js
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import { products } from '../demo-data/products'
+import { Icons } from './Icons';
 
 const Gallery = () => {
   const [productsImages, setProductsImages] = useState([...products]);
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [fileInput, setFileInput] = useState(null);
 
-  const handleDragAndDrop = (results) => {
-    const { source, destination, type } = results;
+
+
+  const sortByIsFeatuedFirstIndex = () => {
+    const featuredProducts = productsImages.filter(product => product.isFeatured);
+    const nonFeaturedProducts = productsImages.filter(product => !product.isFeatured);
+
+    const combinedProducts = [...featuredProducts, ...nonFeaturedProducts];
+    setProductsImages(combinedProducts)
+  }
+
+  useEffect(() => {
+    sortByIsFeatuedFirstIndex()
+
+  }, [])
+
+
+// handle drag and drop 
+  const handleDragAndDrop = (result) => {
+    const { source, destination, type } = result;
 
     if (!destination) return;
 
@@ -20,58 +40,160 @@ const Gallery = () => {
     if (type === "group") {
       const reorderedImages = [...productsImages];
 
-      const storeImagesIndex = source.index;
-      const storeDestinatonIndex = destination.index;
+      const sourceIndex = source.index;
+      const destinationIndex = destination.index;
+      const [draggedItem] = reorderedImages.splice(sourceIndex, 1);
+      const [destinationItem] = reorderedImages.splice(destinationIndex, 1);
+      console.log({ draggedItem })
 
-      const [removedStoreImage] = reorderedImages.splice(storeImagesIndex, 1);
-      reorderedImages.splice(storeDestinatonIndex, 0, removedStoreImage);
+      console.log({ destinationItem })
 
-      return setProductsImages(reorderedImages);
+      if (sourceIndex === 0 || destinationIndex === 0) {
+        draggedItem.isFeatured = !draggedItem.isFeatured;
+        destinationItem.isFeatured = !destinationItem.isFeatured;
+      }
+      reorderedImages.splice(destinationIndex, 0, draggedItem);
+      reorderedImages.splice(sourceIndex, 0, destinationItem);
+
+      setProductsImages(reorderedImages);
     }
-  }
+  };
+
+
+
+
+
+
+
+  // selected
+  const toggleSelect = (itemId) => {
+    if (selectedItems.includes(itemId)) {
+      setSelectedItems(selectedItems.filter((item) => item !== itemId));
+    } else {
+      setSelectedItems([...selectedItems, itemId]);
+    }
+  };
+
+  // delete handle
+  const handleDeleteSelected = () => {
+    setProductsImages(productsImages.filter((item) => !selectedItems.includes(item.id)));
+    setSelectedItems([]);
+  };
+
+// add or upload handle
+  const handleFileChange = (e) => {
+    const files = e.target.files;
+    if (files.length > 0) {
+      const newImages = Array.from(files).map((file, index) => ({
+        id: `new-${Date.now()}-${index}`,
+        src: URL.createObjectURL(file),
+        isFeatured: false,
+      }));
+
+      setProductsImages([...productsImages, ...newImages]);
+      setFileInput(null);
+    }
+  };
 
   return (
     <div className='gallery-wrapper'>
       <div className='gallery-top'>
-        <h1 className='gallery-title'>Gallery</h1>
+        <h1 className='gallery-title'>
+          {selectedItems.length === 0
+            ? 'Gallery'
+            : (
+              <>
+                <input
+                  type='checkbox'
+                  checked={true}
+                  style={{ width: '24px', height: '24px',marginRight:'16px' }}
+                />
+                {selectedItems.length} file{selectedItems.length > 1 ? 's' : ''} selected
+              </>
+            )}
+        </h1>
+        {
+          selectedItems.length > 0 &&
+          <button onClick={handleDeleteSelected} className='images-delete-btn'>
+            {
+              selectedItems.length === 1 ? 'Delete file' : 'Delete files'
+            }
+          </button>
+        }
       </div>
-      <DragDropContext
-        onDragEnd={handleDragAndDrop}
-      >
-        <Droppable droppableId='image-droppable-id' type='group' direction="vertical">
-          {(provided) => (
-            <div {...provided.droppableProps} ref={provided.innerRef} className='image-grid'>
-              {
-                productsImages?.map((item, index) => (
-                  <Draggable draggableId={item.id} key={item.id} index={index}>
-                    {(provided,snapshot) => (
-                      <div 
-                        className="image-grid-item" 
-                        {...provided.dragHandleProps}
-                        {...provided.draggableProps}
-                        ref={provided.innerRef}
-                        style={{
-                          width: snapshot.isDragging ? '200px' : snapshot.draggingOver ? '100px' : 'auto'
-                        
-                        }}
-                      >
-                        <img src={item.src} alt={'image1'} />
-                        {
-                          // console.log(snapshot.isDragging)
-                        provided.placeholder
-                        }
-                      </div>
-                     
-                    )}
-                  </Draggable>
 
-                ))
-              }
+        {
+          productsImages.length === 0 ?
+          <div className='no-data-found'>
+            <h1>No Data Found !</h1>
+          </div>
+
+        :
+        <DragDropContext
+          onDragEnd={handleDragAndDrop}
+        >
+
+          <div className='image-grid'>
+            {
+              productsImages?.map((item, index) => (
+                <Droppable droppableId={`image-droppable-${item.id}`} type='group' direction="vertical" >
+                  {(provided) => (
+                    <div {...provided.droppableProps} ref={provided.innerRef} className={`image-grid-item ${index === 0 ? 'featured' : ''} ${selectedItems.includes(item.id) ? 'selected' : ''}`}>
+                      <Draggable draggableId={item.id} key={item.id} index={index}>
+                        {(provided, snapshot) => (
+                          <div className={`${snapshot.isDragging ? 'is-dragging' : ''}`}
+
+                            {...provided.dragHandleProps}
+                            {...provided.draggableProps}
+                            ref={provided.innerRef}
+
+
+
+                          >
+                            <div className='image-overlay'>
+                              <img src={item.src} alt={'image1'} />
+                            </div>
+
+                            {
+
+                              provided.placeholder
+                            }
+
+                            <label className={`image-checkbox`}>
+                              <input
+                                type='checkbox'
+                                checked={selectedItems.includes(item.id)}
+                                onChange={() => toggleSelect(item.id)}
+                              />
+                            </label>
+                          </div>
+                        )}
+                      </Draggable>
+                    </div>
+                  )}
+                </Droppable>
+              ))
+            }
+            <div className="image-upload">
+              <input
+                multiple
+                type="file"
+                accept="image/*"
+                ref={(input) => setFileInput(input)}
+                onChange={handleFileChange}
+                style={{ display: 'none' }}
+              />
+              <button onClick={() => fileInput && fileInput.click()}>
+                <div className='image-upload-icon'>
+                  {Icons.uploadIcon}
+                </div>
+                Add Image
+              </button>
             </div>
-          
-          )}
-        </Droppable>
-      </DragDropContext>
+          </div>
+        </DragDropContext>
+    
+      }
     </div>
   );
 };
--- src/demo-data/products.js
+++ src/demo-data/products.js
@@ -1,8 +1,15 @@
 import image1 from '../assets/images/image-1.webp'
 import image2 from '../assets/images/image-2.webp'
 import image3 from '../assets/images/image-3.webp'
+import image4 from '../assets/images/image-4.webp'
+import image5 from '../assets/images/image-5.webp'
+import image6 from '../assets/images/image-6.webp'
+import image7 from '../assets/images/image-7.webp'
+import image8 from '../assets/images/image-8.webp'
+import image9 from '../assets/images/image-9.webp'
+import image10 from '../assets/images/image-10.jpeg'
+import image11 from '../assets/images/image-11.jpeg'
 export const products = [
-
     { 
         id: '1', 
         src: image1, 
@@ -16,29 +23,44 @@ export const products = [
     { 
         id: '3', 
         src: image3, 
-        isFeatured: true 
+        isFeatured: false 
     },
     { 
         id: '4', 
-        src: image1, 
-        isFeatured: true 
+        src: image4, 
+        isFeatured: false 
     },
     { 
         id: '5', 
-        src: image3, 
-        isFeatured: true 
+        src: image5, 
+        isFeatured: false 
     },{ 
         id: '6', 
-        src: image2, 
-        isFeatured: true 
+        src: image6, 
+        isFeatured: false 
     },
     { 
         id: '7', 
-        src: image3, 
-        isFeatured: true 
+        src: image7, 
+        isFeatured: false 
     },{ 
         id: '8', 
-        src: image1, 
+        src: image8, 
+        isFeatured: false 
+    },
+    { 
+        id: '9', 
+        src: image9, 
         isFeatured: true 
-    }
+    },{ 
+        id: '10', 
+        src: image10, 
+        isFeatured: false 
+    },
+    { 
+        id: '11', 
+        src: image11, 
+        isFeatured: false 
+    },
+    
 ]
